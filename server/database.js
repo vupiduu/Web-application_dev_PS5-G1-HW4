88f646e2e79fc1d4f0996e3558b9c287ff4f8c89
Karl-Ustav Kõlar <karl-ustav.kolar@askend.com>
2025-12-10 12:40:10 +0200

    Setup backend conf

---
+++ server/database.js
@@ -0,0 +1,46 @@
+const Pool = require('pg').Pool;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+    user: 'postgres',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'testWad',
+    host: 'localhost',
+    port: 5432,
+});
+
+const execute = async(query1, query2) => {
+    try {
+        await pool.connect();
+        await pool.query(query1);
+        await pool.query(query2);
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+}
+
+const createTblQuery1 = `
+    CREATE TABLE IF NOT EXISTS "posttable" (
+        "id" SERIAL PRIMARY KEY,        
+        "body" VARCHAR(200) NOT NULL,
+        "date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );`;
+
+const createTblQuery2 = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password VARCHAR(200) NOT NULL
+    );`;
+
+execute(createTblQuery1, createTblQuery2).then((result) => {
+    if (result) {
+        console.log("Tables created successfully or already exist.");
+    }
+})
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('./database');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+const secret = 'eopjtasofjasl;damsdkla';
+
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: '1h' });
+}
+
