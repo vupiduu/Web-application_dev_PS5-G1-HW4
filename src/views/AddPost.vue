fc697bc26820fff136e84972e5340405b1afba7b
Joonatan Ants Laidus <vupiduu@ut.ee>
2025-12-13 14:23:01 +0200

    added addpost.vue

---
+++ src/views/AddPost.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="add-post">
+    <h2>Add Post</h2>
+
+    <textarea v-model="body" placeholder="Write something..."></textarea>
+
+    <button @click="submitPost">Submit</button>
+  </div>
+</template>
+
+<script>
+import axios from "@/axios";
+
+export default {
+  data() {
+    return {
+      body: ""
+    };
+  },
+  methods: {
+    async submitPost() {
+      await axios.post("/api/posts", { body: this.body });
+      this.$router.push("/");
+    }
+  }
+};
+</script>
